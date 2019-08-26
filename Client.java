3079cf6a9cbf1c143086244f9cc9a95a5ec185e3
Noe <noramgar@protonmail.com>
2019-08-26 01:21:43 -0400

    set up menu for client

--- Client.java
+++ Client.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
 
@@ -7,6 +9,26 @@ public class Client {
         }
 
         String serverHostname = args[0];
-        System.out.println(serverHostname);
+
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println();
+            System.out.println("Commands:");
+            System.out.println("-----------------------------");
+            System.out.println("1. Host current date and time");
+            System.out.println("2. Host uptime");
+            System.out.println("3. Host memory use");
+            System.out.println("4. Host Netstat");
+            System.out.println("5. Host current users");
+            System.out.println("6. Host running processes");
+            System.out.println("7. Quit");
+            System.out.println();
+            System.out.print("Choose an option: ");
+            String input = sc.next();
+
+            if (input.equals("7"))
+                break;
+        }
     }
 }
