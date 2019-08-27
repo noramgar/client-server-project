208b0837736bd3a55cb0c17470ee172a2b224e87
Noe <noramgar@protonmail.com>
2019-08-27 10:43:22 -0400

    set up runCommand and date-time methods

--- Server.java
+++ Server.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.text.DateFormat;
 
 public class Server {
     public static void main(String[] args) throws IOException {
@@ -14,7 +16,38 @@ public class Server {
 
         String inputLine;
         while ((inputLine = in.readLine()) != null) {
-            out.println("Server sends back " + inputLine);
+            //out.println("Server sends back " + inputLine);
+
+            runCommand(Integer.parseInt(inputLine), out);
         }
+    }
+
+    static void runCommand(int command, PrintWriter out) {
+        switch (command) {
+            case 1:
+                String dateAndTime = getDateAndTime();
+                out.println(dateAndTime);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
+
+    static String getDateAndTime() {
+        Date currentDate = new Date();
+        Locale currentLocale = new Locale("en", "US");
+        DateFormat timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+        DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
+        String timeOutput = timeFormatter.format(currentDate);
+        String dateOutput = dateFormatter.format(currentDate);
+        return timeOutput + " - " + dateOutput;
     }
 }
